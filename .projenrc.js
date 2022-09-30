f5ecc4027db5d1a71225f4c15b8d9b44989959b8
Ryoh Kawai <kawairyoh@gmail.com>
2022-09-30 16:18:27 +0000

    Update cdk version

--- .projenrc.js
+++ .projenrc.js
@@ -1,6 +1,6 @@
 const { awscdk } = require('projen');
 const project = new awscdk.AwsCdkTypeScriptApp({
-  cdkVersion: '2.1.0',
+  cdkVersion: '2.44.0',
   defaultReleaseBranch: 'main',
   name: 'workspace',
 
