18b9f68df316b969360cd0a087e8cd0fdf697a18
Ryoh Kawai <kawairyoh@gmail.com>
2022-10-14 14:58:29 +0000

    Rename

--- src/stacks/infrastructure.ts
+++ src/stacks/infrastructure.ts
@@ -1,7 +1,7 @@
 import { Stack, StackProps } from 'aws-cdk-lib';
 import { Construct } from 'constructs';
 
-export class Base extends Stack {
+export class Infrastructure extends Stack {
   constructor(scope: Construct, id: string, props: StackProps = {}) {
     super(scope, id, props);
   }
