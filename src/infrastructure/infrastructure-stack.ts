49fb3fa702ec8fb3cf4413b714b0cf861c6b2840
Ryoh Kawai <kawairyoh@gmail.com>
2022-10-14 16:55:47 +0000

    Refactor network component

--- src/infrastructure/infrastructure-stack.ts
+++ src/infrastructure/infrastructure-stack.ts
@@ -1,73 +1,17 @@
 import { Stack, StackProps } from 'aws-cdk-lib';
 import * as ec2 from 'aws-cdk-lib/aws-ec2';
 import * as elv2 from 'aws-cdk-lib/aws-elasticloadbalancingv2';
-import * as iam from 'aws-cdk-lib/aws-iam';
-import * as logs from 'aws-cdk-lib/aws-logs';
 import { NagSuppressions } from 'cdk-nag';
 import { Construct } from 'constructs';
 import { LogBucket } from './log-bucket';
+import { Network } from './network';
 
 export class InfrastructureStack extends Stack {
   constructor(scope: Construct, id: string, props: StackProps = {}) {
     super(scope, id, props);
 
-    // FlowLogs
-    const logGroup = new logs.LogGroup(this, 'FlowLog');
-    const role = new iam.Role(this, 'FlowLogRole', {
-      assumedBy: new iam.ServicePrincipal('vpc-flow-logs.amazonaws.com'),
-    });
-    // VPC
-    const vpc = new ec2.Vpc(this, 'Vpc', {
-      maxAzs: 2,
-      cidr: '10.0.0.0/16',
-      enableDnsHostnames: true,
-      enableDnsSupport: true,
-      subnetConfiguration: [
-        {
-          name: 'ingress',
-          subnetType: ec2.SubnetType.PUBLIC,
-          cidrMask: 24,
-        },
-        {
-          name: 'application',
-          subnetType: ec2.SubnetType.PRIVATE_WITH_EGRESS,
-          cidrMask: 24,
-        },
-        {
-          name: 'database',
-          subnetType: ec2.SubnetType.PRIVATE_ISOLATED,
-          cidrMask: 24,
-        },
-      ],
-    });
-    vpc.addFlowLog('FlowLog', {
-      destination: ec2.FlowLogDestination.toCloudWatchLogs(logGroup, role),
-    });
-
-    // VPC Endpoint (for ECS)
-    vpc.addInterfaceEndpoint('EcrEndpoint', {
-      service: ec2.InterfaceVpcEndpointAwsService.ECR,
-    });
-    vpc.addInterfaceEndpoint('EcrDkrEndpoint', {
-      service: ec2.InterfaceVpcEndpointAwsService.ECR_DOCKER,
-    });
-    vpc.addInterfaceEndpoint('LogEndpoint', {
-      service: ec2.InterfaceVpcEndpointAwsService.CLOUDWATCH_LOGS,
-    });
-    vpc.addGatewayEndpoint('S3Endpoint', {
-      service: ec2.GatewayVpcEndpointAwsService.S3,
-    });
-    // optional 1 (for use SSM/Secrets)
-    vpc.addInterfaceEndpoint('SSMInterface', {
-      service: ec2.InterfaceVpcEndpointAwsService.SSM,
-    });
-    vpc.addInterfaceEndpoint('SecretsEndpoint', {
-      service: ec2.InterfaceVpcEndpointAwsService.SECRETS_MANAGER,
-    });
-    // optional 2 (for application)
-    vpc.addGatewayEndpoint('DynamoDBEndpoint', {
-      service: ec2.GatewayVpcEndpointAwsService.DYNAMODB,
-    });
+    const network = new Network(this, 'ECSNetwork');
+    const vpc = network.vpc;
 
     // LoadBalancer
     const lb = new elv2.NetworkLoadBalancer(this, 'EcsLoadBalancer', {
--- src/infrastructure/network.ts
+++ src/infrastructure/network.ts
@@ -1,7 +1,67 @@
+import * as ec2 from 'aws-cdk-lib/aws-ec2';
+import * as logs from 'aws-cdk-lib/aws-logs';
 import { Construct } from 'constructs';
 
-export class Network extends Construct {
+export class Network {
+  public readonly vpc: ec2.Vpc;
+
   constructor(scope: Construct, id: string) {
-    super(scope, id);
+
+    // FlowLogs
+    const logGroup = new logs.LogGroup(scope, `${id}-FlowLog`);
+
+    // VPC
+    const vpc = new ec2.Vpc(scope, `${id}-Vpc`, {
+      maxAzs: 2,
+      cidr: '10.0.0.0/16',
+      enableDnsHostnames: true,
+      enableDnsSupport: true,
+      subnetConfiguration: [
+        {
+          name: 'ingress',
+          subnetType: ec2.SubnetType.PUBLIC,
+          cidrMask: 24,
+        },
+        {
+          name: 'application',
+          subnetType: ec2.SubnetType.PRIVATE_WITH_EGRESS,
+          cidrMask: 24,
+        },
+        {
+          name: 'database',
+          subnetType: ec2.SubnetType.PRIVATE_ISOLATED,
+          cidrMask: 24,
+        },
+      ],
+    });
+    vpc.addFlowLog(`${id}-FlowLog`, {
+      destination: ec2.FlowLogDestination.toCloudWatchLogs(logGroup),
+    });
+    this.vpc = vpc;
+
+    // VPC Endpoint (for ECS)
+    vpc.addInterfaceEndpoint('EcrEndpoint', {
+      service: ec2.InterfaceVpcEndpointAwsService.ECR,
+    });
+    vpc.addInterfaceEndpoint('EcrDkrEndpoint', {
+      service: ec2.InterfaceVpcEndpointAwsService.ECR_DOCKER,
+    });
+    vpc.addInterfaceEndpoint('LogEndpoint', {
+      service: ec2.InterfaceVpcEndpointAwsService.CLOUDWATCH_LOGS,
+    });
+    vpc.addGatewayEndpoint('S3Endpoint', {
+      service: ec2.GatewayVpcEndpointAwsService.S3,
+    });
+    // optional 1 (for use SSM/Secrets)
+    vpc.addInterfaceEndpoint('SSMInterface', {
+      service: ec2.InterfaceVpcEndpointAwsService.SSM,
+    });
+    vpc.addInterfaceEndpoint('SecretsEndpoint', {
+      service: ec2.InterfaceVpcEndpointAwsService.SECRETS_MANAGER,
+    });
+    // optional 2 (for application)
+    vpc.addGatewayEndpoint('DynamoDBEndpoint', {
+      service: ec2.GatewayVpcEndpointAwsService.DYNAMODB,
+    });
   }
 }
