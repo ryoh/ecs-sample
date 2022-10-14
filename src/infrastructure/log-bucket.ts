f1559c295b180d601b3f37f3d81a5fdc6701963d
Ryoh Kawai <kawairyoh@gmail.com>
2022-10-14 17:03:36 +0000

    Change class to Construct

--- src/infrastructure/log-bucket.ts
+++ src/infrastructure/log-bucket.ts
@@ -3,11 +3,13 @@ import * as s3 from 'aws-cdk-lib/aws-s3';
 import { NagSuppressions } from 'cdk-nag';
 import { Construct } from 'constructs';
 
-export class LogBucket {
+export class LogBucket extends Construct {
   public readonly bucket: s3.Bucket;
 
   constructor(scope: Construct, id: string) {
-    this.bucket = new s3.Bucket(scope, id, {
+    super(scope, id);
+
+    this.bucket = new s3.Bucket(this, 'LogBucket', {
       blockPublicAccess: s3.BlockPublicAccess.BLOCK_ALL,
       encryption: s3.BucketEncryption.S3_MANAGED,
       enforceSSL: true,
@@ -17,7 +19,7 @@ export class LogBucket {
     NagSuppressions.addResourceSuppressions(this.bucket, [
       {
         id: 'AwsSolutions-S1',
-        reason: 'Bucket for storing access logs, so no access logs are needed for this bucket itself.',
+        reason: 'Bucket for storing access logs, so no needed for itself.',
       },
     ]);
   }
